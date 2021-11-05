b77a6fa1c47f82d2273f459cf717956b6b95228d
Crystopher Carpenter <crystopher_stanley@live.com>
2021-11-05 16:58:00 -0300

    add - cartas virando e desvirando finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 let nCards = 0;
 let nPlays = 0;
+let match = [];
 
 numCards();
 
 function numCards() {
+
     nCards = parseInt(prompt("Com quantas cartas você quer jogar?"));
     while ((nCards % 2) !== 0 || nCards < 4 || nCards > 14) {
         nCards = parseInt(prompt("Com quantas cartas você quer jogar?"));
@@ -13,6 +15,7 @@ function numCards() {
 }
 
 function gameCards() {
+
     const ids = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"];
     const cardsPrint = document.querySelector(".GameBoard");
     const cards = [];
@@ -46,15 +49,42 @@ function comparador() {
 }
 
 function rotateCard(flippedCard) {
+
     const alreadyFlipped = flippedCard.children[0].classList.contains("front-faceRotate")
 
-    flippedCard.children[0].classList.add("front-faceRotate");
-    flippedCard.children[1].classList.add("back-faceRotate");
+    const flippedCardCont = document.querySelectorAll(".front-faceRotate").length;
 
-    if (alreadyFlipped) {
+    if (flippedCardCont < 2) {
+        flippedCard.children[0].classList.add("front-faceRotate");
+        flippedCard.children[1].classList.add("back-faceRotate");
+
+        let card1 = ((document.querySelectorAll(".front-faceRotate")[0]).parentElement.id);
+        let card2 = ((document.querySelectorAll(".front-faceRotate")[1]).parentElement.id);
+        console.log(card1, card2);
+
+        if (card1 === card2) {
+            return;
+        } else {
+            setTimeout(unflipCards, 1000);
+        }
+    }
+
+
+
+    if (alreadyFlipped || flippedCardCont > 1) {
         return;
     }
     nPlays++;
-
     console.log(nPlays);
+}
+
+function unflipCards() {
+
+    for (let i = 0; i < 2; i++) {
+        let unflipFront = document.querySelector(".front-faceRotate");
+        unflipFront.classList.remove("front-faceRotate");
+
+        let unflipBack = document.querySelector(".back-faceRotate");
+        unflipBack.classList.remove("back-faceRotate");
+    }
 }
