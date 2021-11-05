5cfe1ee815c24f3c81bbdb0d36e24946f46242d2
Crystopher Carpenter <crystopher_stanley@live.com>
2021-11-05 12:05:16 -0300

    add distribuição das cartas concluída

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,50 @@
+let nCards = 0;
 
-function nCards() {
-    let nCards = parseInt(prompt("Com quantas cartas você quer jogar?"));
+numCards();
+
+function numCards() {
+    nCards = parseInt(prompt("Com quantas cartas você quer jogar?"));
     while ((nCards % 2) !== 0 || nCards < 4 || nCards > 14) {
         nCards = parseInt(prompt("Com quantas cartas você quer jogar?"));
     }
+
     gameCards();
 }
 
 function gameCards() {
-    const array = document.getElementsByClassName("card")
+    const ids = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"];
+    const cardsPrint = document.querySelector(".GameBoard");
+    const cards = [];
 
-    array[0].classList.add("hide");
+    for (let i = 0; i < (nCards / 2); i++) {
+        for (let j = 0; j < 2; j++) {
 
-    console.log(array[0])
+            cards.push(`<div class="card" id="${ids[i]}" data-identifier="card" onclick="rotateCard(this)">
+            <div class="front-face face" data-identifier="card">
+                <img src="assets/front.png" alt="frente">
+            </div>
+            <div class="back-face face" data-identifier="back-face">
+                <img src="assets/${ids[i]}parrot.gif" alt="verso">
+            </div>
+        </div>
+    `);
+        }
+    }
+
+    cards.sort(comparador);
+
+    for (let i = 0; i < cards.length; i++) {
+
+        cardsPrint.innerHTML += cards[i];
+    }
+}
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+
+function rotateCard(flippedCard) {
+    flippedCard.children[0].classList.add("front-faceRotate");
+
+    flippedCard.children[1].classList.add("back-faceRotate");
 }
